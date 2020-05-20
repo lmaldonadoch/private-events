b1e0382e2d315b6811153ede26d58dda40ff0934
shubham14p3 <shubham14p3@gmail.com>
2020-05-21 03:24:56 +0530

    Added Creator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_164220) do
+ActiveRecord::Schema.define(version: 2020_05_20_214625) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.time "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
