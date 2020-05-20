673e270817faef9bbe4f52c109fed82db3192f1d
shubham14p3 <shubham14p3@gmail.com>
2020-05-21 02:30:38 +0530

    Created User Controller with responds to new create and show action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+	def sign_in(user)
+		session[:user_id]=user.id
+	end
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+	end
+	def signed_in?
+		!current_user.nil?
+	end
+	def sign_out
+		session.delete(:user_id) if signed_in?
+		@current_user = nil
+	end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+	def new
+		@user=User.new
+	end
+
+	def create
+    @user = User.new(user_params)
+    if @user.save
+    #   sign_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+   end
+	def show
+		if signed_in?
+			@user=User.find_by(id: session[:user_id])
+		else
+			redirect_to signin_path
+		end
+	end
+	# def index
+	# 	@user=User.paginate(page:params[:page])
+	# end
+	private
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+end
