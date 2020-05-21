d7796c1e59977021e318f29393a2868b03140b1a
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-21 11:37:09 -0500

    Validated the invitation creation and asignation. Modified show page for event to show the users invited to the event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
 	# has_many: user
 	belongs_to :creator, class_name: "User"
-	has_many :attendees, through: :invites, source: :event_attendee
-    has_many :invites
+	has_many :attendees, through: :invites, source: :user
+  has_many :invites
 end
